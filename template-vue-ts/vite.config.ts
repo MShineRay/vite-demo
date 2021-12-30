c6ec0668ff44df2d02b784c5ccf6fd30842d8f88
msr <mshineray@qq.com>
2021-12-30 17:50:13 +0800

    upd: 多页面

--- template-vue-ts/vite.config.ts
+++ template-vue-ts/vite.config.ts
@@ -1,3 +1,4 @@
+import {resolve} from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,5 +7,11 @@ export default defineConfig({
   plugins: [vue()],
   build:{
     // cssCodeSplit: false // false整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+    rollupOptions: { // 多页面
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
   }
 })
